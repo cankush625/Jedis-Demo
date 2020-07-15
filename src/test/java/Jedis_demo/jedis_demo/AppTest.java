1d27199792af4cda7442a4367f84088078ec97b9
Ankush Chavan <cankush625@gmail.com>
2020-07-15 14:59:27 +0530

    Test for checking redis server connection
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- src/test/java/Jedis_demo/jedis_demo/AppTest.java
+++ src/test/java/Jedis_demo/jedis_demo/AppTest.java
@@ -1,20 +1,19 @@
 package Jedis_demo.jedis_demo;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import redis.clients.jedis.Jedis;
+
+public class AppTest {
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test() {
+    	Jedis jedis = new Jedis("127.0.0.1", 6379);
+    	String result = jedis.set("hello", "world");
+    	assertEquals(result, "OK");
+    	String value = jedis.get("hello");
+        assertEquals(value, "world");
+        jedis.close();
     }
 }
